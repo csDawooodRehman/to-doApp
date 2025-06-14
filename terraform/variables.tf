cc36d0fd9c4098276089022ccd7a29d569adb85a
Dawood Rehman <dawoodrehman3283@gmail.com>
2025-06-14 21:31:36 +0500

    two corrected paths 4th

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,19 @@
-variable "aws_region"      { default = "us-east-1" }
-variable "instance_type"   { default = "t2.micro" }
-variable "key_name"        { default = "swarm-key" }
-variable "public_key_path" { default = "terraform\swarm-key.pub"}
-variable "ami_id"          { default = "ami-084568db4383264d4" }
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "swarm-key"
+}
+
+variable "public_key_path" {
+  default = "terraform/swarm-key.pub"  
+}
+
+variable "ami_id" {
+  default = "ami-084568db4383264d4"
+}
