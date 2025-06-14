b8f8b1eedd679c978d4c572ff15d62f6c67cdb47
Dawood Rehman <dawoodrehman3283@gmail.com>
2025-06-14 20:20:09 +0500

    two corrected s3 three

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "dawood-terraform-state-bucket"
     key          = "todo-swarm/terraform.tfstate"
-    region       = "us-east-1"    
+    region       = "eu-north-1"    
     encrypt      = true
     use_lockfile = true             
   }
