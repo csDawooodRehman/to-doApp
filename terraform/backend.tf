8544e1958d22e1f98c5d1ddcdc80b5584522df7d
Dawood Rehman <dawoodrehman3283@gmail.com>
2025-06-14 23:03:49 +0500

    final 7

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "daud-terraform-state-bucket"
-    key          = "todo-swarm/terraform.tfstate"
-    region       = "us-east-1"    
-    encrypt      = true
-\            
+    bucket = "daud-terraform-state-bucket"
+    key    = "todo-swarm/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
   }
 }
